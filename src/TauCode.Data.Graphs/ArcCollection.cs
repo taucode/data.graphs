4994f628a48c5735b95adf000d1f3846b010d27a
Andrey Kovalenko <ak@taucode.com>
2022-06-24 22:34:05 +0300

    Resurrected & added vertices and arcs.

---
+++ src/TauCode.Data.Graphs/ArcCollection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TauCode.Data.Graphs
+{
+    internal class ArcCollection : IReadOnlyCollection<IArc>
+    {
+        #region Fields
+
+        private readonly HashSet<IArc> _arcs;
+
+        #endregion
+
+        #region ctor
+
+        internal ArcCollection(IVertex vertex)
+        {
+            this.Vertex = vertex ?? throw new ArgumentNullException(nameof(vertex));
+            _arcs = new HashSet<IArc>();
+        }
+
+        #endregion
+
+        #region Internal
+
+        internal IVertex Vertex { get; }
+
+        internal void AddArc(IArc arc)
+        {
+            _arcs.Add(arc);
+        }
+
+        internal void RemoveArc(IArc arc)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IReadOnlyCollection<IArc> Members
+
+        public IEnumerator<IArc> GetEnumerator() => _arcs.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => _arcs.GetEnumerator();
+
+        public int Count => _arcs.Count;
+
+        #endregion
+    }
+}
---
+++ src/TauCode.Data.Graphs/Graph.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TauCode.Data.Graphs
+{
+    public class Graph : IGraph
+    {
+        #region Fields
+
+        private readonly HashSet<IVertex> _vertices;
+
+        #endregion
+
+        #region ctor
+
+        public Graph()
+        {
+            _vertices = new HashSet<IVertex>();
+        }
+
+        public Graph(IEnumerable<IVertex> vertices)
+        {
+            if (vertices == null)
+            {
+                throw new ArgumentNullException(nameof(vertices));
+            }
+
+            _vertices = new HashSet<IVertex>(vertices);
+        }
+
+        #endregion
+
+        #region Pvivate
+
+        private void AddPrivate(IVertex vertex)
+        {
+            if (vertex == null)
+            {
+                throw new ArgumentNullException(nameof(vertex));
+            }
+
+            if (_vertices.Contains(vertex))
+            {
+                throw new InvalidOperationException("Graph already contains this vertex.");
+            }
+
+            _vertices.Add(vertex);
+        }
+
+        #endregion
+
+        #region ISet<IVertex> Members
+
+        bool ISet<IVertex>.Add(IVertex vertex)
+        {
+            this.AddPrivate(vertex);
+            return true;
+        }
+
+        public void ExceptWith(IEnumerable<IVertex> other) => _vertices.ExceptWith(other);
+
+        public void IntersectWith(IEnumerable<IVertex> other) => _vertices.IntersectWith(other);
+
+        public bool IsProperSubsetOf(IEnumerable<IVertex> other) => _vertices.IsProperSubsetOf(other);
+
+        public bool IsProperSupersetOf(IEnumerable<IVertex> other) => _vertices.IsProperSupersetOf(other);
+
+        public bool IsSubsetOf(IEnumerable<IVertex> other) => _vertices.IsSubsetOf(other);
+
+        public bool IsSupersetOf(IEnumerable<IVertex> other) => _vertices.IsSupersetOf(other);
+
+        public bool Overlaps(IEnumerable<IVertex> other) => _vertices.Overlaps(other);
+
+        public bool SetEquals(IEnumerable<IVertex> other) => _vertices.SetEquals(other);
+
+        public void SymmetricExceptWith(IEnumerable<IVertex> other) => _vertices.SymmetricExceptWith(other);
+
+        public void UnionWith(IEnumerable<IVertex> other) => _vertices.UnionWith(other);
+
+        #endregion
+
+        #region ICollection<IVertex> Members
+
+        public int Count => _vertices.Count;
+
+        public bool IsReadOnly => ((ICollection<IVertex>)_vertices).IsReadOnly;
+
+        public void Add(IVertex vertex) => this.AddPrivate(vertex);
+
+        public void Clear() => _vertices.Clear();
+
+        public bool Contains(IVertex vertex)
+        {
+            if (vertex == null)
+            {
+                throw new ArgumentNullException(nameof(vertex));
+            }
+
+            return _vertices.Contains(vertex);
+        }
+
+        public void CopyTo(IVertex[] array, int arrayIndex) => _vertices.CopyTo(array, arrayIndex);
+
+        public bool Remove(IVertex vertex)
+        {
+            if (vertex == null)
+            {
+                throw new ArgumentNullException(nameof(vertex));
+            }
+
+            return _vertices.Remove(vertex);
+        }
+
+        #endregion
+
+        #region IEnumerable<IVertex> Members
+
+        public IEnumerator<IVertex> GetEnumerator() => _vertices.GetEnumerator();
+
+        #endregion
+
+        #region IEnumerable Members
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        #endregion
+    }
+}
